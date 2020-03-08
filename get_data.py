d1d60d5e64d9bd37a511e9bbdee77155eb6ea4b5
Ricard Trinchet <37812127+rtrinchet@users.noreply.github.com>
2020-03-08 19:00:31 +0100

    Rearrange get_data structure

--- get_data.py
+++ get_data.py
@@ -123,7 +123,7 @@ def combine_df(url_teams, dict_team_url):
     '''
     Combine the datasets obtained from each iteration of get_data
     '''
-
+    print('INFO: getting players 2K data')
     df_list = []
     for url in url_teams:
         df = get_data(url, dict_team_url[url])
@@ -136,7 +136,7 @@ def get_stats(url):
 
     # get contents
     soup = get_soup(url)
-
+    print('INFO: getting stats data')
     col_names = []
     for i in range(1, len(soup.tbody.find_all('tr')[1].find_all('td'))+1):
         col_names.append(soup.thead.find_all(
@@ -171,41 +171,3 @@ def get_stats(url):
     stats_df = stats_df.replace(r'^\s*$', np.nan, regex=True)
 
     return stats_df
-
-
-# ## Ratings ----------
-# start getting  list of the url where we can obtain all the data: one URL per team
-url = 'https://nba2k19.2kratings.com/current-teams-on-nba-2k19'
-teams_dict = get_teams_dict(url)
-ratings = combine_df(teams_dict, teams_dict)
-
-# # Creamos el fichero
-# ratings.to_csv("2k19_ratings.csv", index=False)
-
-
-# Stats for 17-18 season --------
-# normal
-stats_1718 = get_stats(
-    'https://www.basketball-reference.com/leagues/NBA_2018_per_game.html')
-# advanced
-stats_1718_adv = get_stats('https://www.basketball-reference.com/leagues/NBA_2018_advanced.html')
-
-
-# merge
-complete_stats_1718 = pd.merge(stats_1718, stats_1718_adv, how='inner', on=["Player", 'Pos', 'Age', 'Tm', 'G'])
-
-complete_stats_1718.columns = ['Player', 'Pos', 'Age', 'Tm', 'G', 'GS', 'MPG', 'FG', 'FGA', 'FG.pct',
-       '3P', '3PA', '3P.pct', '2P', '2PA', '2P.pct', 'eFG.pct', 'FT', 'FTA',
-       'FT.pct', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS',
-       'MP_Total', 'PER', 'TS.pct', '3PAr', 'FTr', 'ORB.pct', 'DRB.pct', 'TRB.pct',
-       'AST.pct', 'STL.pct', 'BLK.pct', 'TOV.pct', 'USG.pct', 'empty1', 'OWS',
-       'DWS', 'WS', 'WS/48', 'empty2', 'OBPM', 'DBPM', 'BPM', 'VORP']
-# drop columns with NA created with the scraping part
-stats  = complete_stats_1718.drop(['empty1', 'empty2'], axis = 1)
-
-# stats.to_csv("2k19_ratings.csv", index=False)
-
-df = pd.merge(ratings, stats, how='inner', left_on="name", right_on="Player").drop('Player', axis = 1) # inner join: less results but easier to clean
-
-
-df.to_csv("2019_ratings_stats.csv", index=False)
---
+++ test.py
@@ -0,0 +1,42 @@
+
+from get_data import get_teams_dict, get_soup, get_data, combine_df, get_stats
+import pandas as pd
+# ## Ratings ----------
+# start getting  list of the url where we can obtain all the data: one URL per team
+url = 'https://nba2k19.2kratings.com/current-teams-on-nba-2k19'
+teams_dict = get_teams_dict(url)
+ratings = combine_df(teams_dict, teams_dict)
+
+# # Creamos el fichero
+# ratings.to_csv("2k19_ratings.csv", index=False)
+
+
+# Stats for 17-18 season --------
+# normal
+stats_1718 = get_stats(
+    'https://www.basketball-reference.com/leagues/NBA_2018_per_game.html')
+# advanced
+stats_1718_adv = get_stats('https://www.basketball-reference.com/leagues/NBA_2018_advanced.html')
+
+
+# merge
+complete_stats_1718 = pd.merge(stats_1718, stats_1718_adv, how='inner', on=["Player", 'Pos', 'Age', 'Tm', 'G'])
+
+complete_stats_1718.columns = ['Player', 'Pos', 'Age', 'Tm', 'G', 'GS', 'MPG', 'FG', 'FGA', 'FG.pct',
+       '3P', '3PA', '3P.pct', '2P', '2PA', '2P.pct', 'eFG.pct', 'FT', 'FTA',
+       'FT.pct', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS',
+       'MP_Total', 'PER', 'TS.pct', '3PAr', 'FTr', 'ORB.pct', 'DRB.pct', 'TRB.pct',
+       'AST.pct', 'STL.pct', 'BLK.pct', 'TOV.pct', 'USG.pct', 'empty1', 'OWS',
+       'DWS', 'WS', 'WS/48', 'empty2', 'OBPM', 'DBPM', 'BPM', 'VORP']
+# drop columns with NA created with the scraping part
+stats  = complete_stats_1718.drop(['empty1', 'empty2'], axis = 1)
+
+# stats.to_csv("2k19_ratings.csv", index=False)
+
+df = pd.merge(ratings, stats, how='inner', left_on="name", right_on="Player").drop('Player', axis = 1) # inner join
+
+
+df.to_csv("2019_ratings_stats.csv", index=False)
+
+print(df.head().T)
+
